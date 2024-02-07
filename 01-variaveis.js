0236b194bdfc7ec55397a039a9d46f9767fef515
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-07 11:26:08 -0300

    javascript-basico

---
+++ 01-variaveis.js
@@ -0,0 +1,13 @@
+//Criando variáveis com "var" (var nome _variavel = <valor>;)
+var nome = "Rayssa" //var cria variavel global
+var idade = 16
+
+console.log("Meu nome é" , nome, "e tenho" , idade, "anos!");
+console.log("Agora fiz aniversário! Tenho" , idade+1, "anos!");
+
+//aumentando automaticamente +1 (autoincremento)
+idade++ //idade = idade + 1;
+console.log("Agora fiz aniversário! Tenho" , idade, "anos!");
+
+
+
---
+++ 02-aritmetico.js
@@ -0,0 +1,32 @@
+//utilizando operadores aritmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8+7);
+
+//efetuar a soma de dois números informados por você (var nome variavel = 12)
+
+var x = 9
+var y = 3
+console.log("A soma entre", x , "e", y, "é:", x+y);
+
+var soma = x + y
+var subtracao = x-y
+var multiplicacao= x * y;
+var divisao = x / y
+
+console.log("Soma = ", soma);
+console.log("subtracao = ",subtracao);
+console.log("multiplicacao = ", multiplicacao);
+console.log("divisao = ", divisao);
+
+//Regras para nomear identificadores (var, const ou let)
+//Podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20;
+const _minhaConstante = 3.1514;
+var $minhaVariavel = 11;
+
+
+  
+
+
