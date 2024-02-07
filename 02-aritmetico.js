d4ac9b02e5c384cc922ca1202d110ce4588491f0
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-07 14:52:16 -0300

    Exercício

--- 02-aritmetico.js
+++ 02-aritmetico.js
@@ -26,6 +26,15 @@ var outraVariavel = 20;
 const _minhaConstante = 3.1514;
 var $minhaVariavel = 11;
 
+var v1 = 10;
+var v2 = 3;
+console.log("------------------------------");
+console.log("Valores: v1 =", v1, " | v2 =", v2);
+console.log("------------------------------");
+console.log("Resto da divisão inteira: v1 % v2 =", v1 % v2);
+console.log("Exponenciação: v1**v2 (v1 elevado à v2=)", v1 ** v2);
+
+
 
   
 
