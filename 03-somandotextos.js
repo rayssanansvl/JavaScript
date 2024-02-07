6d199862dc108b7c15869fb6b69e2167afc4fce8
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-07 17:03:08 -0300

    finalizado atividades em sala de aula

---
+++ 03-somandotextos.js
@@ -0,0 +1,17 @@
+//somando texto
+
+var nome = "Rayssa"
+var boasVindas= "Olá " + nome + ", seja bem vindo!";
+
+
+console.log(boasVindas);
+console.log(boasVindas);
+console.log(boasVindas);
+console.log(boasVindas);
+console.log(boasVindas);
+
+var texto1 = "Olá";
+var texto2 = "Bom dia";
+
+console.log(texto1 + texto2);
+console.log(texto1 + ", " + texto2);
