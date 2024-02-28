dcc35997dfc55f1ef769fc2eae4b59ef76df652b
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-28 10:22:35 -0300

    Atividade-5-funções-Rayssa.js

---
+++ Atividade-5-Rayssa.js
@@ -0,0 +1,14 @@
+//Crie uma função que receba como parâmetro o ano de nascimento de uma pessoa a retorne sua idade.
+
+function idade(anoNascimento) {
+    return 2024 - anoNascimento;
+}
+console.log(`A minha idade nesse ano de 2024 é de ${idade (2007)} anos`);
+
+//Crie uma função que receba a quantidade de combustível de um carro e retorne quantos Kms ele pode viajar com esse combustível.
+//Considere que o veículo faz uma média de 12 Kms/L
+
+function Km(combustivel) {
+    return 12 * combustivel;
+}
+console.log(`O carro poderá viajar cerca de: ${Km(50)} kms/L`);
