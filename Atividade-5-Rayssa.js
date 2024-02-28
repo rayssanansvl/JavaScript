b3bdbf756e3d23a1c227cc0180731919989ffdc8
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-28 10:38:11 -0300

    Atividade-5-funções

--- Atividade-5-Rayssa.js
+++ Atividade-5-Rayssa.js
@@ -8,7 +8,7 @@ console.log(`A minha idade nesse ano de 2024 é de ${idade (2007)} anos`);
 //Crie uma função que receba a quantidade de combustível de um carro e retorne quantos Kms ele pode viajar com esse combustível.
 //Considere que o veículo faz uma média de 12 Kms/L
 
-function Km(combustivel) {
+function media(combustivel) {
     return 12 * combustivel;
 }
-console.log(`O carro poderá viajar cerca de: ${Km(50)} kms`);
+console.log(`O carro poderá viajar cerca de: ${media(50)} kms`);
