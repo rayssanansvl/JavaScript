bb2dbdee8f988885dffd91bfb2dd03310a9cd95e
Rayssa <rayssa.silveira@portalsesisp.org.br>
2024-02-28 17:00:33 -0300

    Atividade-5-Rayssa

---
+++ 15-condicional-if.js
@@ -0,0 +1,71 @@
+//condição simples única
+
+var anoNascimento = 2007;
+
+if (anoNascimento > 2003)
+    console.log(`A pessoa nasceu depois de 2003!`);
+
+if (anoNascimento < 2003)
+    console.log(`A pessoa nasceu antes de 2003!`);
+
+if (anoNascimento == 2003)
+    console.log(`A pessoa nasceu em 2003!`);
+
+//Condicional simples if-else
+var anoNascimento = 2002;
+if (anoNascimento > 2000)
+    console.log(`A pessoa nasceu depois de 2000!`)
+else
+    console.log(`A pessoa nasceu antes de 2000!`)
+
+//Condicional composto com if
+const login = "admin"; //digitado pelo usuario
+var senha = "123456";  //digitado pelo usuario
+
+//--------------------------------------------
+    //banco de dados
+    const loginUser = "admin"; //banco de dados
+    const senhaUser = "admin"; //banco de dados
+//--------------------------------------------
+if (login == loginUser && senha == senhaUser) //&& significa E -- || significa OU
+    console.log(`Acesso permitido!`);
+else console.log(`Acesso negado!`);
+
+//Função sinaleiro 
+function sinaleiro(cor){
+ if (cor == "vermelho")
+    console.log(`Pare`);
+ else if (cor == "amarelo")
+    console.log(`Preste atenção`);
+ else if (cor == "verde")
+    console.log(`Siga`);
+ else
+    console.log(`Você informou cores diferente de verde, amarelo ou vermelho!`);
+}
+sinaleiro("vermelho");
+
+//Desvio condicional if com bloco de comandos
+console.log(`-----------------------------------------------`);
+var idade = 18;
+
+if (idade <= 18){
+    console.log("entrou no if...");
+    idade++, console.log("Incrementei a idade...");
+    console.log(`A nova idade é ${idade}`);
+    console.log(`Agora estou saindo do bloco de codigos if`);
+}
+console.log(`Terminei!`);
+
+
+//Desvio condicional if inline (ternário)
+    //validação (algo == algo) ou (algo > algumacoisa)
+    //? (IF)
+    //Se for verdadeiro
+    // : (ELSE)
+    //se for falso
+    // testedeValidacao == teste ? "Bla bla" : ("Ble ble")
+var preco = 100;
+
+var resultado = preco <= 100 ? "tá barato" : "Vish ta caro!"
+
+console.log(resultado);
--- Atividade-5-Rayssa.js
+++ Atividade-5-Rayssa.js
@@ -13,3 +13,40 @@ function media(combustivel) {
 }
 
 console.log(`Um carro de 50 litros poderá viajar cerca de ${media(50)} kms`);
+
+//Crie uma função que receba a idade de uma pessoa e retorne uma frase dizendo se ela pode votar ou não.
+console.log("");
+console.log("-------------------------------------------------------");
+var idade = 16;
+function votacao (idade){
+    if (idade < 16)
+        console.log("Você não pode votar!");
+    else if (idade >= 16 && idade <18){
+        console.log("Você pode votar, mas não é obrigatório");
+}
+    else{
+        console.log("Você pode votar");
+    }
+}
+votacao(16);
+
+console.log("------------------------------------------------------");
+
+function votacao2(idade){
+    switch (true) {
+        case idade < 16:
+            console.log(`Você não pode votar!`);
+            break;
+        case idade >= 16 && idade < 18:
+            console.log(`Você pode votar, mas não é obrigatório!`);
+             break;
+        default:
+            console.log(`Você pode votar!`);
+            break;
+    }
+}
+
+votacao2(10);
+
+
+
